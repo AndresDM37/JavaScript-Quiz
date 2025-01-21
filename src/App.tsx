23e8d6842ca237d75b0d1b51f244d85f67f69b2e
andresmarchenaporras@gmail.com <Andres37DM>
2025-01-20 22:36:25 -0500

    ultimos cambios

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Container, Typography, Stack } from "@mui/material";
 import "./App.css";
 import { JavaScriptLogo } from "./JavaScriptLogo";
+import { GitHubIcon } from "./GitHubLogo";
 import { Start } from "./Start";
 import { useQuestionStore } from "./store/questions";
 import { Game } from "./Game";
@@ -27,6 +28,28 @@ function App() {
 
         <h3>Programa hecho con React ⚛️</h3>
         <h3>Por Andrés Marchena</h3>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <a
+            href="https://github.com/AndresDM37/JavaScript-Quiz"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <GitHubIcon />
+          </a>
+          <style>
+            {`
+              a:hover {
+              transform: scale(1.1);
+              }
+            `}
+          </style>
+        </div>
 
         {questions.length > 0 && <Game />}
       </Container>
